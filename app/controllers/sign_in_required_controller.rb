8c586d278a88c8e866875e41cfafaedf1f846778
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-04 16:52:05 -0600

    use default time zone in app

--- app/controllers/sign_in_required_controller.rb
+++ app/controllers/sign_in_required_controller.rb
@@ -2,4 +2,12 @@ class SignInRequiredController < ApplicationController
   include UserSessionHelper
 
   before_action :require_signed_in_user
+
+  around_filter :user_time_zone
+
+  private
+
+  def user_time_zone(&block)
+    Time.use_zone(Rails.application.config.time_zone, &block)
+  end
 end
